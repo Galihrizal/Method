443d75a3b10a1e2645527c76ee33aa51c7c57c00
galihrizal <galih;.2301@students.amikom.ac.id>
2019-04-02 14:40:43 +0700

    method dan overloading

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // deklarasi variabel
+            int hasil;
+
+            // membuar objek dari class Calculator
+            Calculator calculator = new Calculator();
+
+            // panggil method Penjumlahan
+            hasil = calculator.Penjumlahan(10, 2);
+            CetakHasil("Hasil Penjumalahan : " + hasil);
+
+            // panggil method Penjumlahan dengan 3 parameter (overload method)
+            hasil = calculator.Penjumlahan(10, 2, 3);
+            CetakHasil("Hasil Penjumlahan Overload : " + hasil);
+
+            // panggil method Pengurangan
+            hasil = calculator.Pengurangan(7, 2);
+            CetakHasil("Hasil Pengurangan : " + hasil);
+
+            // panggil static method, langsung dari nama classnya
+            hasil = Calculator.Perkalian(5, 2);
+            CetakHasil("Hasil Perkalian : " + hasil);
+
+            Console.ReadKey();
+        }
+        static void CetakHasil(string hasil)
+        {
+            Console.WriteLine(hasil);
+        }
+    }
+}
---
+++ calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Method
+{
+    public class Calculator
+    {
+        public int Penjumlahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        // overload method
+        public int Penjumlahan(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        //method static
+        public static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
